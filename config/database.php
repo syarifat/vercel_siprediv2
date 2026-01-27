7f285fe394ee665baa85bcf00f7cd81a4309ab54
syarifat <syarifahsanit@gmail.com>
2026-01-27 13:39:52 +0700

    ssl database 1

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'engine' => null,
             'options' => [
                 PDO::MYSQL_ATTR_SSL_CA => base_path('\storage\isrgrootx1.pem'),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ],
         ],
 
